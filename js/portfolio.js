e947b78fe39bbb31e9e7731c4a89091122969284
Ztenbritt <116632945+Ztenbritt@users.noreply.github.com>
2025-05-31 18:59:55 +0200

    Add files via upload

---
+++ js/portfolio.js
@@ -0,0 +1,35 @@
+const fmj1Element = document.getElementById("fmj1");
+
+    fmj1Element.addEventListener("click", function (event) {
+       fmj1Element.style.width = "60%";
+    })
+
+    const fmj2Element = document.getElementById("fmj2");
+
+    fmj2Element.addEventListener("click", function (event) {
+       fmj2Element.style.width = "60%";
+    })
+
+    const fmj3Element = document.getElementById("fmj3");
+
+    fmj3Element.addEventListener("click", function (event) {
+       fmj3Element.style.width = "60%";
+    })
+
+    const fmj4Element = document.getElementById("fmj4");
+
+    fmj4Element.addEventListener("click", function (event) {
+       fmj4Element.style.width = "60%";
+    })
+
+    const fmj5Element = document.getElementById("fmj5");
+
+    fmj5Element.addEventListener("click", function (event) {
+       fmj5Element.style.width = "60%";
+    })
+
+    const fmj6Element = document.getElementById("fmj6");
+
+    fmj6Element.addEventListener("click", function (event) {
+       fmj6Element.style.width = "60%";
+    })
